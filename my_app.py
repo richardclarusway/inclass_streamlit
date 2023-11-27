461ff45b3270edf0ac50ef3224cfe00b132c6fa4
richardclarusway <45499397+richardclarus@users.noreply.github.com>
2023-11-27 10:20:28 +0300

    inclass

--- my_app.py
+++ my_app.py
@@ -1,159 +1,47 @@
-# Streamlit Documentation: https://docs.streamlit.io/
-
-
 import streamlit as st
-import pandas as pd
-import numpy as np
-from PIL import Image  # to deal with images (PIL: Python imaging library)
-
-# Title/Text
-st.title("This is a title")
-st.text("This is some test.")
-
-# Markdown
-st.markdown("Streamlit is **_really_ cool** :+1:")
-st.markdown("# This is a markdown")
-st.markdown("## This is a markdown")
-st.markdown("### This is a markdown")
-
-# Header/Subheader
-st.header('This is a header')
-st.subheader('This is a subheader')
-
-# Success/Info/Error
-st.success('This is a success message!')
-st.info('This is a purely informational message')
-st.error("This is an error.")
-st.warning("This is a warning message!")
-st.exception("NameError('name there is not defined')")
-
-# Help
-st.help(range)
-
-# Write
-st.write("Hello World! :sunglasses:")
-st.write(range(10))
-
-# Add image
-img = Image.open("images.jpeg")
-st.image(img, caption="cattie", width=300)
-
-# Add video
-
-#my_video = open("ml.mov",'rb')
-#st.video(my_video)
-
-# Add youtube video
-st.video("https://www.youtube.com/watch?v=uHKfrz65KSU")
-
-# Add checkbox
-st.checkbox("Up and Down")
-cbox= st.checkbox("Hide and Seek")
-
-if cbox :
-    st.write("Hide")
-else :
-    st.write("Seek")
-
-
-# Add radio button
-status = st.radio("Select a color",("blue","orange","yellow"))
-st.write("My favorite color is ", status)
-
-# Add button
-st.button("Click me")
-
-if st.button("Press me") :
-    st.success("Analyze Results are..")
-
-# Add select box
-occupation=st.selectbox("Your Occupation", ["Programmer", "DataScientist", "Doctor"])
-st.write("Your Occupation is ", occupation)
-
-# Multi_select
-multi_select = st.multiselect("Select multiple numbers",[1,2,3,4,5])
-st.write(f"You selected {len(multi_select)} number(s)")
-st.write("Your selection is/are", multi_select)
-for i in range(len(multi_select)):
-    st.write(f"Your {i+1}. selection is {multi_select[i]}")
-
-# Slider
-option1 = st.slider("Select a number", min_value=5, max_value=70, value=30, step=5)
-option2 = st.slider("Select a number", min_value=0.2, max_value=30.2, value=5.2, step=0.2)
-
-result=option1*option2
-st.write("multiplication of two options is:",result)
-
-# Text_input
-name = st.text_input("Enter your name", placeholder="Your name here")
-if st.button("Submit"):
-    st.write("Hello {}".format(name.title()))
-    
-# Code  # to show as if code
-st.code("import pandas as pd")
-st.code("import pandas as pd\nimport numpy as np")
-
-# Echo  # it is used "with block" to draw some code on the app, then execute it
-with st.echo():
-    import pandas as pd
-    import numpy as np
-    df = pd.DataFrame({"a":[1,2,3], "b":[4,5,6]})
-    df
-
-
-# Date input
-import datetime
-today=st.date_input("Today is", datetime.datetime.now())
-date=st.date_input("Enter the date")
-
-# Time input
-the_time=st.time_input("The time is", datetime.time(8, 45))
-hour=st.time_input(str(pd.Timestamp.now()))
-st.write("Hour is", hour)
-
-# Sidebar
-st.sidebar.title("Sidebar title")
-st.sidebar.header("Sidebar header")
-
-# Sidebar with slider
-a=st.sidebar.slider("input",0,5,2,1)
-x=st.sidebar.slider("input2")
-st.write("# sidebar input result")
-st.success(a*x)
-
-# Dataframe
-df=pd.read_csv("Advertising.csv")
-
-# To display dataframe there are 3 methods
-
-# Method 1
-st.table(df.head())
-# Method 2
-st.write(df.head())  # dynamic, you can sort
-st.write(df.isnull().sum())
-# Method 3
-st.dataframe(df.describe().T)  # dynamic, you can sort
-
-# To load machine learning model
 import pickle
-filename = "my_model"
-model=pickle.load(open(filename, "rb"))
+import pandas as pd
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OrdinalEncoder
 
-# To take feature inputs
-TV = st.sidebar.number_input("TV:",min_value=5, max_value=300)
-radio = st.sidebar.number_input("radio:",min_value=1, max_value=50)
-newspaper = st.sidebar.number_input("newspaper:",min_value=0, max_value=120)
+st.sidebar.title('Car Price Prediction')
+html_temp = """
+<div style="background-color:blue;padding:10px">
+<h2 style="color:white;text-align:center;">Streamlit ML Cloud App </h2>
+</div>"""
+st.markdown(html_temp, unsafe_allow_html=True)
 
-# Create a dataframe using feature inputs
-my_dict = {"TV":TV,
-           "radio":radio,
-           "newspaper":newspaper}
+
+age=st.sidebar.selectbox("What is the age of your car:",(0,1,2,3))
+hp=st.sidebar.slider("What is the hp_kw of your car?", 40, 300, step=5)
+km=st.sidebar.slider("What is the km of your car", 0,350000, step=1000)
+gearing_type=st.sidebar.radio('Select gear type',('Automatic','Manual','Semi-automatic'))
+car_model=st.sidebar.selectbox("Select model of your car", ('Audi A1', 'Audi A3', 'Opel Astra', 'Opel Corsa', 'Opel Insignia', 'Renault Clio', 'Renault Duster', 'Renault Espace'))
+
+
+ds13_model=pickle.load(open("rf_model_new","rb"))
+ds13_transformer = pickle.load(open('transformer', 'rb'))
+
+
+my_dict = {
+    "age": age,
+    "hp_kW": hp,
+    "km": km,
+    'Gearing_Type':gearing_type,
+    "make_model": car_model
+    
+}
 
 df = pd.DataFrame.from_dict([my_dict])
+
+
+st.header("The configuration of your car is below")
 st.table(df)
 
-# Prediction with user inputs
-predict = st.button("Predict")
-result = model.predict(df)
-if predict :
-    st.success(result[0])
+df2 = ds13_transformer.transform(df)
+
+st.subheader("Press predict if configuration is okay")
+
+if st.button("Predict"):
+    prediction = ds13_model.predict(df2)
+    st.success("The estimated price of your car is €{}. ".format(int(prediction[0])))
